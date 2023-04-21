82bece0760df999717e1c7d77ad980b728c541dd
Replit user <>
2023-04-21 15:48:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -117,7 +117,7 @@ Pair * firstTreeMap(TreeMap * tree) {
 
 Pair * nextTreeMap(TreeMap * tree) {
   if(tree->current->right != NULL)
-    return tree->current->pair;
+    return tree->current->right->pair->key;
   else{  
     if(tree->lower_than(tree->current, tree->current->pair->key) == 1)
       return tree->current->pair->key;
