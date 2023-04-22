37b65076674ebf0ca6745a9e354f3138227042e0
Replit user <>
2023-04-22 23:39:51 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -176,7 +176,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
   else{
     while(tree->current != NULL){
       if(tree->lower_than(tree->current->pair->key, key) == 1){
-        ub_node = nextTreeMap(tree);
+        return nexTreeMap(tree);
         //ub_node = tree->current;
         //es el nodo siguiente
       }
