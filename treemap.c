c92032b33e1d233b5a45756206ad1628983b124c
Replit user <>
2023-04-22 19:29:39 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -122,8 +122,8 @@ void removeNode(TreeMap * tree, TreeNode* node) {
       if(tree->current->left != NULL && tree->current->right != NULL){
         aux = minimum(tree->current->right);
         tree->current->pair = aux->pair;
-        if(aux->parent)
-          aux->parent->right = NULL;
+        aux->parent->right = NULL;
+        aux->parent = NULL;
         //removeNode(tree, aux);
       }
     }
