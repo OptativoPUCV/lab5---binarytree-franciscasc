113518e104c28dd5373efaddee84ae45841038d1
Replit user <>
2023-04-23 02:30:55 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -170,7 +170,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 Pair * upperBound(TreeMap * tree, void* key) {
   Pair *aux = searchTreeMap(tree, key);
 
-  if (aux != NULL) {
+  if (aux == NULL) {
     if (tree->lower_than(tree->current->pair->key, key) == 1) {
       if( nextTreeMap(tree) == NULL) 
         return NULL;
